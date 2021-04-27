a799a17db50673c1b20d3d301bb4f3b8466fd09f
Assassinationrogue <saumyadip11@gmail.com>
2021-04-27 14:36:13 +0530

    support window added

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const button1 = document.querySelector('.back-this-project');
 const bookmarkButton = document.querySelector('.bookmark');
+const backThisProjectWindow = document.querySelector('.pledge_button');
+const closeBtn = document.querySelector('.close_btn');
+const secondContainer = document.querySelector('.container_2');
+const bookmark = document.querySelector('.bookmark');
 
 let count = 0;
-
 button1.addEventListener('click',(clicked)=>{
-    count++;
-    let currEvent = count % 2 === 0 ? 'disabled':'enabled';
-    console.log(currEvent)
+    backThisProjectWindow.style.visibility = 'visible';
+    secondContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.37)';
 });
+
+closeBtn.addEventListener('click',(clicked)=>{
+    backThisProjectWindow.style.visibility = 'hidden';
+})
 
 
 bookmarkButton.addEventListener('click',(clicked)=>{
     count++;
-    let currEvent = count % 2 === 0 ? 'disabled':'enabled';
-    console.log(currEvent)
+    let currEvent = count % 2 === 0 ? 'hidden':'visible';
+    bookmark.style.visibility = currEvent;
+    bookmark.style.content = 'Bookmarked'
 })
+
